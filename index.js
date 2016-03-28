6d9fca2413f8597c4b48f6be7ce8f4dc980eefe4
LeReunionais <reunionaisle@gmail.com>
2016-03-28 20:09:50 +0800

    Use service_js lib

--- index.js
+++ index.js
@@ -7,17 +7,21 @@ var io = require('socket.io').listen(server);
 
 var subscriber = require('./lib/subscriber.js');
 
-const PORT = 6000;
-subscriber.connect(PORT);
+var REGISTRY_HOST = process.env.REGISTRY_HOST;
+if (REGISTRY_HOST === undefined || REGISTRY_HOST === '') {
+	throw new Error('REGISTRY_HOST environment variable was not defined. Please define it.');
+}
 
 io.on('connection', (socket) => {
+	subscriber.connect(REGISTRY_HOST);
 	subscriber.on((message) => {
 		socket.emit('state', message);
 	});
 });
-
 app.use(express.static('app'));
 
 server.listen(9000, () => {
 	console.log("Listening on port 9000");
 });
+
+
